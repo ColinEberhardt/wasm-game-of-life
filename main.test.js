55d591132c330df6abed9204f84ab73746f99564
Colin Eberhardt <colin.eberhardt@gmail.com>
2018-04-18 22:09:09 +0100

    implemented live neighbour count (and moved to 32 bit per cell)

--- main.test.js
+++ main.test.js
@@ -8,6 +8,18 @@ const instantiate = async () => {
   return instance.exports;
 };
 
+const dumpMemory = () => {
+  const memory = new Uint32Array(wasm.memory.buffer, 0, 50 * 50);
+  let asciiMem = "";
+  for (let y = 0; y < 49; y++) {
+    for (let x = 0; x < 49; x++) {
+      asciiMem += wasm.getCell(x, y) > 0 ? "#" : ".";
+    }
+    asciiMem += "\n";
+  }
+  console.log(asciiMem);
+};
+
 let wasm;
 
 beforeAll(() => {
@@ -21,8 +33,8 @@ beforeEach(async done => {
 
 test("offsetFromCoordinate", () => {
   expect(wasm.offsetFromCoordinate(0, 0)).toBe(0);
-  expect(wasm.offsetFromCoordinate(49, 0)).toBe(49);
-  expect(wasm.offsetFromCoordinate(10, 2)).toBe(10 + 2 * 50);
+  expect(wasm.offsetFromCoordinate(49, 0)).toBe(49 * 4);
+  expect(wasm.offsetFromCoordinate(10, 2)).toBe(10 * 4 + 2 * 200);
 });
 
 test("get / set cell", () => {
@@ -31,8 +43,46 @@ test("get / set cell", () => {
   expect(wasm.getCell(2, 2)).toBe(1);
 });
 
+test("set cell does not leak to neighbours", () => {
+  wasm.setCell(2, 2, 1);
+  expect(wasm.getCell(3, 2)).toBe(0);
+  expect(wasm.getCell(1, 2)).toBe(0);
+});
+
+test("memory starts in an empty state", () => {
+  for (let x = 0; x < 49; x++) {
+    for (let y = 0; y < 49; y++) {
+      expect(wasm.getCell(x, y)).toBe(0);
+    }
+  }
+});
+
 test("read memory directly", () => {
-  const memory = new Uint8Array(wasm.memory.buffer, 0, 4096);
+  const memory = new Uint32Array(wasm.memory.buffer, 0, 50 * 50);
   wasm.setCell(2, 2, 10);
   expect(memory[2 + 2 * 50]).toBe(10);
+  expect(memory[3 + 2 * 50]).toBe(0);
+});
+
+test("without boundary values", () => {
+  // starts at zero
+  expect(wasm.liveNeighbourCount(2, 2)).toBe(0);
+
+  // add each neighbour in turn
+  wasm.setCell(1, 1, 1);
+  expect(wasm.liveNeighbourCount(2, 2)).toBe(1);
+  wasm.setCell(2, 1, 1);
+  expect(wasm.liveNeighbourCount(2, 2)).toBe(2);
+  wasm.setCell(3, 1, 1);
+  expect(wasm.liveNeighbourCount(2, 2)).toBe(3);
+  wasm.setCell(3, 2, 1);
+  expect(wasm.liveNeighbourCount(2, 2)).toBe(4);
+  wasm.setCell(1, 2, 1);
+  expect(wasm.liveNeighbourCount(2, 2)).toBe(5);
+  wasm.setCell(1, 3, 1);
+  expect(wasm.liveNeighbourCount(2, 2)).toBe(6);
+  wasm.setCell(2, 3, 1);
+  expect(wasm.liveNeighbourCount(2, 2)).toBe(7);
+  wasm.setCell(3, 3, 1);
+  expect(wasm.liveNeighbourCount(2, 2)).toBe(8);
 });
--- main.wat
+++ main.wat
@@ -2,9 +2,11 @@
   (memory (export "memory") 1)
   (func $offsetFromCoordinate (param $x i32) (param $y i32) (result i32)
     get_local $y
-    i32.const 50
+    i32.const 200
     i32.mul
     get_local $x
+    i32.const 4
+    i32.mul
     i32.add
   )
   (func $setCell (param $x i32) (param $y i32) (param $value i32)
@@ -18,9 +20,85 @@
     get_local $x
     get_local $y
     call $offsetFromCoordinate
-    i32.load
+    i32.load8_u
+  )
+  (func $liveNeighbourCount (param $x i32) (param $y i32) (result i32)
+    i32.const 0
+
+    ;; add the cell value from x + 1, y
+    get_local $x
+    i32.const 1
+    i32.add
+    get_local $y
+    call $getCell
+    i32.add
+
+    ;; add the cell value from x - 1, y
+    get_local $x
+    i32.const -1
+    i32.add
+    get_local $y
+    call $getCell
+    i32.add
+
+    ;; add the cell value from x, y - 1
+    get_local $x
+    get_local $y
+    i32.const -1
+    i32.add
+    call $getCell
+    i32.add
+
+    ;; add the cell value from x - 1, y - 1
+    get_local $x
+    i32.const -1
+    i32.add
+    get_local $y
+    i32.const -1
+    i32.add
+    call $getCell
+    i32.add
+
+    ;; add the cell value from x + 1, y - 1
+    get_local $x
+    i32.const 1
+    i32.add
+    get_local $y
+    i32.const -1
+    i32.add
+    call $getCell
+    i32.add
+
+    ;; add the cell value from x, y + 1
+    get_local $x
+    get_local $y
+    i32.const 1
+    i32.add
+    call $getCell
+    i32.add
+
+    ;; add the cell value from x - 1, y + 1
+    get_local $x
+    i32.const -1
+    i32.add
+    get_local $y
+    i32.const 1
+    i32.add
+    call $getCell
+    i32.add
+
+    ;; add the cell value from x + 1, y + 1
+    get_local $x
+    i32.const 1
+    i32.add
+    get_local $y
+    i32.const 1
+    i32.add
+    call $getCell
+    i32.add
   )
   (export "offsetFromCoordinate" (func $offsetFromCoordinate))
+  (export "liveNeighbourCount" (func $liveNeighbourCount))
   (export "getCell" (func $getCell))
   (export "setCell" (func $setCell))
 )
